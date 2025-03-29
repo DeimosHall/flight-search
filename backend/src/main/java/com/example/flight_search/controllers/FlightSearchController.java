66645150cb4ee5429967f7a6376f6e77a50df0f5
deimos <deimoshall@gmail.com>
2025-03-29 16:35:40 -0600

    chore: use api keys inside docker container

--- backend/src/main/java/com/example/flight_search/controllers/FlightSearchController.java
+++ backend/src/main/java/com/example/flight_search/controllers/FlightSearchController.java
@@ -1,5 +1,6 @@
 package com.example.flight_search.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class FlightSearchController {
+
+    final String AMADEUS_API_KEY = System.getenv("AMADEUS_API_KEY");
+    final String AMADEUS_API_SECRET = System.getenv("AMADEUS_API_SECRET");
 
     @GetMapping("/health")
     public String healthCheck() {
